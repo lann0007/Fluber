3b82acc2206026c72ac60b9f060e04de3aaa04de
Luke Derby <derb0010@flinders.edu.au>
2022-09-25 14:16:20 +0930

    feat: init test driver profile and driver user

---
+++ backend/src/api/driver-profile/controllers/driver-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * driver-profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::driver-profile.driver-profile');
---
+++ backend/src/api/driver-profile/services/driver-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * driver-profile service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::driver-profile.driver-profile');
