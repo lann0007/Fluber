86bbbaa0334cb503775182f748268bf6d46f6660
Luke Derby <derb0010@flinders.edu.au>
2022-09-26 06:45:43 +0930

    chore: get driver profile deep populate relations

--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -21,6 +21,25 @@
         </tr>
       </tbody>
     </q-markup-table>
+
+    <q-separator v-if="mappedDriverProfile" class="q-my-md" />
+    <q-expansion-item
+      v-if="mappedDriverProfile"
+      label="Driver Profile"
+      header-style="font-size: 1.5em; border: solid 1px #ddd"
+    >
+      <q-markup-table wrap-cells flat bordered>
+        <tbody>
+          <tr v-for="d in mappedDriverProfile" :key="d.key">
+            <td style="width: 35%">{{ d.key }}</td>
+            <td style="width: 65%">{{ d.value }}</td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+      <q-separator class="q-my-md" />
+      <h6>Identification</h6>
+      <q-img :src="driverIdentification" />
+    </q-expansion-item>
   </div>
 </template>
 
@@ -34,6 +53,7 @@ export default {
   name: 'Profile-page',
   async mounted() {
     await this.fetchUserPfp()
+    await this.fetchDriverProfile()
   },
   setup() {
     const authStore = useAuthStore()
@@ -47,16 +67,33 @@ export default {
       { key: 'Email', value: userData.email },
       { key: 'Member since', value: userData.createdAt },
     ]
+
     return {
       mappedUserData,
+      mappedDriverProfile: null,
+      driverIdentification: null,
       //source: https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png
       profilePicture: require('src/assets/placeholder_pfp.png'),
       newPfp: null
     }
   },
   methods: {
+    async fetchDriverProfile () {
+      if(this.authStore.user.role.type === 'driver') {
+        this.mappedDriverProfile = [
+          { key: 'First name', value: this.authStore.user.driverProfile.firstName },
+          { key: 'Last name', value: this.authStore.user.driverProfile.lastName },
+          { key: 'License number', value: this.authStore.user.driverProfile.licenseNumber },
+          { key: 'Vehicle registration', value: this.authStore.user.driverProfile.vehicleRegistration },
+          { key: 'Driver since', value: this.authStore.user.driverProfile.createdAt },
+        ]
+
+        //we simply populate the relation to the identification image, which itself
+        //stores a link to a blob container
+        this.driverIdentification = `${c.coreApiBaseUrl}${this.authStore.user.driverProfile.identification.url}`
+      }
+    },
     async uploadPfp() {
-      console.log('newPfp: ', this.newPfp)
       const formData = new FormData()
       formData.append('files', this.newPfp, this.newPfp.name)
 
@@ -96,20 +133,24 @@ export default {
       }
     },
     async fetchUserPfp() {
-      const userData = await axios
-        .get(`${c.coreApiBaseUrl}/api/users/me?populate=profilePicture`, {
-          headers: {
-            Authorization: `Bearer ${this.authStore.authToken}`
-          }
-        })
-        .catch((err) => {
-          console.error(err.response)
-        })
-
-      //we simply populate the relation to the image, which itself stores a link to a
-      //blob container
-      if (userData.data.profilePicture) {
-        this.profilePicture = `${c.coreApiBaseUrl}${userData.data.profilePicture.url}`
+      if (!this.authStore.user.profilePicture) {
+        //double-check api for pfp (should have been gotten on login)
+        const userData = await axios
+          .get(`${c.coreApiBaseUrl}/api/users/me?populate=profilePicture`, {
+            headers: {
+              Authorization: `Bearer ${this.authStore.authToken}`
+            }
+          })
+          .catch((err) => {
+            console.error(err.response)
+          })
+        //we simply populate the relation to the image, which itself stores a link to a
+        //blob container
+        if (userData.data.profilePicture) {
+          this.profilePicture = `${c.coreApiBaseUrl}${userData.data.profilePicture.url}`
+        }
+      } else {
+        this.profilePicture = `${c.coreApiBaseUrl}${this.authStore.user.profilePicture.url}`
       }
     },
   },
--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -11,7 +11,8 @@ export const useAuthStore = defineStore('auth', {
   persist: true,
   actions: {
     async doLogin(email, password) {
-      const resp = await axios
+      //this is just for the JWT
+      const res = await axios
         .post(`${c.coreApiBaseUrl}/api/auth/local`, {
           identifier: email,
           password: password
@@ -21,10 +22,26 @@ export const useAuthStore = defineStore('auth', {
           notifyHandler('negative', 'Failed to login', err.response.statusText)
           return false
         })
-
       //assume login request to API was successful
-      this.authToken = resp.data.jwt
-      this.user = resp.data.user
+      this.authToken = res.data.jwt
+
+      //also try get the driver profile (can't `populate` on `auth/local`)
+      const populateStr = 'populate[driverProfile][populate][0]=identification&populate[role][populate][0]=*&populate[profilePicture][populate][0]=*'
+      const resp = await axios
+        .get(`${c.coreApiBaseUrl}/api/users/me?${populateStr}`, {
+          headers: {
+            Authorization: `Bearer ${this.authToken}`,
+          }
+        })
+        .catch((err) => {
+          console.error(err.response)
+          notifyHandler('negative', 'Failed to login', err.response.statusText)
+          //if this request fails, the whole login flow was unsuccessful, so reset
+          this.authToken = null
+          return false
+        })
+      this.user = resp.data
+
       return true
     },
     doLogout() {
