f1ed64c3da7c61e8aa829d37b1e01993119febbc
Luke Derby <derb0010@flinders.edu.au>
2022-09-27 07:21:01 +0930

    refactor: break out getting user profile; better error handling on login

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -19,35 +19,29 @@ export const useAuthStore = defineStore('auth', {
         })
         .catch((err) => {
           console.error(err.response)
-          notifyHandler('negative', 'Failed to login', err.response.statusText)
-          return false
+          notifyHandler('negative', 'Failed to login', err.response.data.error.message)
         })
+      if(!res) return false
+
       //assume login request to API was successful
       this.authToken = res.data.jwt
 
       //also try get the driver profile (can't `populate` on `auth/local`)
-      const populateStr = 'populate[driverProfile][populate][0]=identification&populate[role][populate][0]=*&populate[profilePicture][populate][0]=*'
-      const resp = await axios
-        .get(`${c.coreApiBaseUrl}/api/users/me?${populateStr}`, {
-          headers: {
-            Authorization: `Bearer ${this.authToken}`,
-          }
-        })
+      const success = await this.getUserProfile()
         .catch((err) => {
-          console.error(err.response)
-          notifyHandler('negative', 'Failed to login', err.response.statusText)
+          notifyHandler('negative', 'Failed to login. Could not retrieve the user profile', err.message)
           //if this request fails, the whole login flow was unsuccessful, so reset
           this.authToken = null
-          return false
+          console.error(err)
         })
-      this.user = resp.data
-
-      return true
+      //`success` will be undefined if an error is caught, so cast to boolean
+      return !!success
     },
     doLogout() {
       this.authToken = null
       this.user = null
     },
+    //TODO also do call to `api/users/me` like in `doLogin()` (reuse the code)
     async doRegister(username, email, password) {
       const resp = await axios
         .post(`${c.coreApiBaseUrl}/api/auth/local/register`, {
@@ -71,6 +65,26 @@ export const useAuthStore = defineStore('auth', {
     },
     setUser(user) {
       this.user = user
+    },
+    /**
+     * Gets the user profile while populating necessary relations
+     * 
+     * @returns {Boolean} if the request was successful
+     */
+    async getUserProfile() {
+      const populateStr = 'populate[driverProfile][populate][0]=identification&populate[role][populate][0]=*&populate[profilePicture][populate][0]=*'
+      const resp = await axios
+        .get(`${c.coreApiBaseUrl}/api/users/me?${populateStr}`, {
+          headers: {
+            Authorization: `Bearer ${this.authToken}`,
+          }
+        })
+        .catch((err) => {
+          console.error(err.response)          
+          throw new Error(err.response.data.error.message)
+        })
+      this.user = resp.data
+      return true
     }
   },
 })
