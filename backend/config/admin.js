8b425c2f94d04482734d09a61feb61b3f44c6005
Hayden Graham <haydengraham101@outlook.com>
2022-09-27 09:20:10 +0930

    Updated the backend variables

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: "HEicYYCD5NlF7Zd7CXOAlFIJa5gmPazk",
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: "NElw53Xh3qSLkn7YPlPBDj6NPhy4QmRB",
+    salt: env('API_TOKEN_SALT'),
   },
 });
--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys:["mHVoLNh2U5","WDqVWWM4sv"],
+    keys: env.array('APP_KEYS'),
   },
 });
