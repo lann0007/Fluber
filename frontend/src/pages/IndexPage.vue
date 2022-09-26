34c950bf71a485b5e610989739d882095161c91f
Hayden Graham <haydengraham101@outlook.com>
2022-09-26 12:58:47 +0930

    adding the js code from leaflet causes blank page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -19,6 +19,11 @@
 
 <script>
 import { defineComponent } from "vue";
+let map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
 
 export default defineComponent({
   name: "IndexPage",
