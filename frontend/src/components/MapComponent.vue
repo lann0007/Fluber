5b0af82398af606f9d1c0228b2573709be385ca6
Luke Derby <derb0010@flinders.edu.au>
2022-09-27 09:53:30 +0930

    chore: basic map showing on page

---
+++ frontend/src/components/MapComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- source: https://vue2-leaflet.netlify.app/quickstart/#hello-map -->
+  <!-- for some reason if this is wrapped in another HTML element it won't render, so any
+    styles and layout should be done on this `MapComponent` (it's parent) -->
+  <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker} from '@vue-leaflet/vue-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159]
+    }
+  }
+}
+</script>
