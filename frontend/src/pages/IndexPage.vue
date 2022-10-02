9784a31d2ba7adcbdce58e5496f1c95468a9b58b
Luke Derby <derb0010@flinders.edu.au>
2022-10-02 16:56:26 +1030

    chore: remove lipsum

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -2,29 +2,18 @@
   <q-page class="column items-center">
     <h1>Fluber Home</h1>
     <!-- TODO is the home page going straight to the map? -->
-    <p class="q-px-md">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-      vehicula purus. Suspendisse eu lacus purus. Nunc ut varius nulla. Ut
-      pharetra quam vitae dolor lacinia, vel elementum enim fringilla. Quisque
-      convallis tellus in interdum blandit. Ut nec dolor nisl. Aenean lobortis
-      massa ut faucibus tempus. Quisque feugiat, metus id laoreet sagittis, eros
-      ante suscipit ipsum, sit amet porta magna magna at odio. Vivamus at odio
-      mi. Praesent eu pharetra massa. Aliquam ipsum nulla, vehicula id gravida
-      eget, interdum at metus. Proin placerat nunc eu nisl auctor consequat.
-    </p>
-
     <MapComponent style="width: 90%" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import MapComponent from "../components/MapComponent.vue";
+import { defineComponent } from 'vue'
+import MapComponent from '../components/MapComponent.vue'
 
 export default defineComponent({
-  name: "IndexPage",
+  name: 'IndexPage',
   components: {
     MapComponent,
   },
-});
+})
 </script>
