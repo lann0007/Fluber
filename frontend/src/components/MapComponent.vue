5dc07faa2f259ecfcd650b6488e1bee9744d9a07
Luke Derby <derb0010@flinders.edu.au>
2022-10-02 14:54:04 +1030

    fix: erroring when trying to load infinite tiles; use user's position

--- frontend/src/components/MapComponent.vue
+++ frontend/src/components/MapComponent.vue
@@ -2,10 +2,17 @@
   <!-- source: https://vue2-leaflet.netlify.app/quickstart/#hello-map -->
   <!-- for some reason if this is wrapped in another HTML element it won't render, so any
     styles and layout should be done on this `MapComponent` (it's parent) -->
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
+  <l-map 
+    v-if="userCoords !== null"
+    style="height: 300px"
+    :zoom="19"
+    :maxZoom="19"
+    :center="userCoords"
+  >
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
+    <l-marker :lat-lng="userCoords"></l-marker>
   </l-map>
+  <h2 v-else>Waiting for GPS</h2>
 </template>
 
 <script>
@@ -18,14 +25,51 @@ export default {
     LTileLayer,
     LMarker
   },
+  mounted() {
+    this.startLocationUpdates()
+  },
+  unmounted() {
+    this.stopLocationUpdates()
+  },
   data () {
     return {
+      //leaflet stuff
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
+      //general data
+      watchPositionId: null,
+      userCoords_: null,
+    }
+  },
+  computed: {
+    userCoords: {
+      set(val) {
+        console.log('setting userCoords: ', val)
+        this.userCoords_ = val
+      },
+      get() {
+        console.log('getting userCoords: ', this.userCoords_)
+        return this.userCoords_
+      }
+    }
+  },
+  methods: {
+    startLocationUpdates() {
+      this.watchPositionId = navigator.geolocation.watchPosition(
+        (pos) => {
+          this.userCoords = {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          }
+        },
+        (err) => {
+          console.error('navigator experienced error watching position: ', err)
+        }
+      )
+    },
+    stopLocationUpdates() {
+      navigator.geolocation.clearWatch(this.watchPositionId)
     }
   }
 }
